b8ec056b10ab9057355719a8c191207ce384bb47
Maciej <rozek@o2.pl>
2025-01-10 15:33:56 +0100

    Initial database setup and  data types created.

---
+++ src/types/types.ts
@@ -0,0 +1,36 @@
+export interface Part {
+    id: number;
+    name: string;
+    price: number;
+    quantity?: number; 
+    partId: string;
+    category: string;
+}
+
+export interface Category {
+    id: string;
+    name: string;
+    identifier: string;
+    position: number;
+}
+export interface Order {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    value: number;
+    details: string;
+    items: OrderItem[];
+    total: number;
+}
+export interface InputValues {
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+export interface OrderItem {
+    productId: number;
+    quantity: number;
+    price: number;
+    name?: string; 
+}
